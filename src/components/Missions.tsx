cda6e875848b4a5d7e06dada572bce0710f9ebb9
JyojiTenguam <j.y.oji@hotmail.com>
2024-02-28 10:33:59 -0300

    Resolvendo requisito 10 de Exercise Solar System

--- src/components/Missions.tsx
+++ src/components/Missions.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionsCard from './MissionCard';
 
 function Missions() {
   return (
     <div data-testid="missions">
       <Title headline="Missões" />
+      {
+        missions.map((mission) => (
+          <MissionsCard
+            key={ mission.name }
+            name={ mission.name }
+            year={ mission.year }
+            country={ mission.country }
+            destination={ mission.destination }
+          />
+        ))
+      }
     </div>
   );
 }
